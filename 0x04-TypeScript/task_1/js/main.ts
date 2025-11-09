b70805886cbe7ea5b37669befc638b0788706e20
dahsiee <chuksgabriella2019@gmail.com>
2025-11-09 15:27:03 +0000

    add Directors interface and example instance to main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,27 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allows additional properties
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
